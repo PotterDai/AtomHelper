62964645e4049cfc66b831a81c551c203d530a2a
Potter Dai <potterdaica@gmail.com>
2017-04-28 22:50:38 -0400

    Fix app name.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AtomHelper.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :atom_helper,
+    [app: :atomhelper,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
@@ -15,7 +15,7 @@ defmodule AtomHelper.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: []]
   end
 
   # Dependencies can be Hex packages:
