9caea830afc121bd123ccb6b99e93a0bf9ad9b50
Potter Dai <potterdaica@gmail.com>
2017-04-28 22:57:35 -0400

    Add basic atom <-> string conversion. Add specs.

--- lib/atom_helper.ex
+++ lib/atom_helper.ex
@@ -2,12 +2,18 @@ defmodule AtomHelper do
   @moduledoc """
   Documentation for AtomHelper.
   """
-  def atom_keys_to_str(map) when is_map(map), do: for {key, val} <- map, into: %{}, do: {Atom.to_string(key), val}
-  def atom_keys_to_str(arr) when is_list(arr), do: for val <- arr, into: [], do: Atom.to_string(val)
+  @spec atom_to_str(atom | map | list) :: binary
+  def atom_to_str(atom) when is_atom(atom), do: Atom.to_string(atom)
+  def atom_to_str(map) when is_map(map), do: for {key, val} <- map, into: %{}, do: {Atom.to_string(key), val}
+  def atom_to_str(arr) when is_list(arr), do: for val <- arr, into: [], do: Atom.to_string(val)
 
-  def str_keys_to_atom(map) when is_map(map), do: for {key, val} <- map, into: %{}, do: {String.to_atom(key), val}
-  def str_keys_to_atom(arr) when is_list(arr), do: for val <- arr, into: [], do: String.to_atom(val)
+  @spec str_to_atom(binary | map | list) :: atom
+  def str_to_atom(str) when is_binary(str), do: String.to_atom(str)
+  def str_to_atom(map) when is_map(map), do: for {key, val} <- map, into: %{}, do: {String.to_atom(key), val}
+  def str_to_atom(arr) when is_list(arr), do: for val <- arr, into: [], do: String.to_atom(val)
 
-  def str_keys_to_atom!(map) when is_map(map), do: for {key, val} <- map, into: %{}, do: {String.to_existing_atom(key), val}
-  def str_keys_to_atom!(arr) when is_list(arr), do: for val <- arr, into: [], do: String.to_existing_atom(val)
+  @spec str_to_atom!(binary | map | list) :: atom
+  def str_to_atom!(str) when is_binary(str), do: String.to_existing_atom(str)
+  def str_to_atom!(map) when is_map(map), do: for {key, val} <- map, into: %{}, do: {String.to_existing_atom(key), val}
+  def str_to_atom!(arr) when is_list(arr), do: for val <- arr, into: [], do: String.to_existing_atom(val)
 end
